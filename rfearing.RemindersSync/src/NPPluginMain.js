1bf8511a0f6b242c9c5ee4b1128f512f09cddfcc
rfearing <rfearing@github.com>
2023-02-15 15:59:33 -0500

    List reminders

--- rfearing.RemindersSync/src/NPPluginMain.js
+++ rfearing.RemindersSync/src/NPPluginMain.js
@@ -1,19 +1,29 @@
 import pluginJson from '../plugin.json'
+import { createTask, createTitle } from './support/helpers'
 import { logError, JSP } from '@helpers/dev'
 
+// List all lists and their reminders
 export async function listAllReminders() {
   try {
     const titles = await Calendar.availableReminderListTitles()
-    titles.forEach(async title => {
+    for (const title of titles) {
+      await createTitle(title)
       const reminders = await Calendar.remindersByLists([title])
-      console.log(`${title  } >>>>>>>>>>`)
-      reminders.forEach((reminder) => {
-        console.log(`${reminder.title  } isCompleted: ${reminder.isCompleted}`)
-      })
-      console.log(`${title}`)
-    })
-    console.log('DONE')
+      for (const reminder of reminders) {
+        if (reminder.isCompleted) {
+          continue
+        }
+        await createTask(reminder.title)
+      }
+
+    }
   } catch (error) {
     logError(pluginJson, JSP(error))
   }
 }
+
+// List all reminders from a specific list
+
+// Update reminders
+
+// Create reminders
